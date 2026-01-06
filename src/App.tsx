840d7fd3846b46bfcbd1433591a13d0d3f9b73ae
CAFE2l <pciskolargx@gmail.com>
2026-01-06 20:58:27 +0800

    portfolio created

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css' // Importando o CSS (se tiver)
+
+function App() {
+  //Você pode usar variáveis do JS no HTML!
+
+  return (
+    <body>
+      <header>
+
+      </header>
+      <main>
+          <h1>Em breve...</h1>
+      </main>
+      <footer>
+
+      </footer>
+    </body>
+  )
+}
+
+export default App
